004e0040977edc218fc7b954a2420d895675b244
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2019-12-31 18:57:33 +0300

    header seperated

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'blog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],  #template directory adreslemesi
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')] #statik dosyaların directorysini belirttik
